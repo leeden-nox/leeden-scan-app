3068cc2f05bb7118d5245f4094b9bdea27d0116c
wongtaozhe <taozhe.wong@leedonnox.com>
2025-09-05 14:59:56 +0800

    20250905 - add register sw

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import ReactDOM from 'react-dom';
 import App from './App'
 import { createRoot } from 'react-dom/client';
 import 'antd/dist/reset.css';
+import { registerSW } from 'virtual:pwa-register';
+
+if ('serviceWorker' in navigator) {
+  registerSW();
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
