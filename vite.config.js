806ee48cf9aa374cdf0b5535acef9ed314c84faa
wongtaozhe <taozhe.wong@leedonnox.com>
2025-09-05 11:50:34 +0800

    20250905 - add pwa configurations

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["logo.png"],
+      manifest: {
+        name: "Leeden Scan app PWA",
+        short_name: "Leeden Scan app",
+        description: "To replace old leeden scanner and CMS scanner",
+        start_url: "/",
+        display: "standalone"
+      }
+    })
+  ],
   server: {
     port: 4000
   }
