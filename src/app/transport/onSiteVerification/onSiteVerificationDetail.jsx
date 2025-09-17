a83b33282750ff9c6fe6b20b65246620697ea7c6
Darren Ng <darren.ng@apac.nipponsanso.com>
2025-09-17 17:05:56 +0800

    osv hide loading

--- src/app/transport/onSiteVerification/onSiteVerificationDetail.jsx
+++ src/app/transport/onSiteVerification/onSiteVerificationDetail.jsx
@@ -109,7 +109,7 @@ const columns = [
       onRefresh={() => initial()}
     >
       <>
-        <SpinLoading />
+        {/* <SpinLoading /> */}
         <Table
           dataSource={data}
           columns={columns}
